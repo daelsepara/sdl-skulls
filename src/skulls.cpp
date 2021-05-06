d87541abe6f3015003f9ecf1eebd8089fa348adf
Dael Separa <dael.separa@gmail.com>
2021-05-06 12:54:31 +0800

    re-arrange includes

--- src/skulls.cpp
+++ src/skulls.cpp
@@ -1,10 +1,12 @@
-// Using SDL and standard IO
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+// Standard IO
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+
+// Using SDL
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 // Screen dimension constants
 const int SCREEN_WIDTH = 800;
