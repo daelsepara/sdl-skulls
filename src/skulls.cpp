77f038ed187a8ea225dcdf352d887e7b79717114
Dael Separa <dael.separa@gmail.com>
2021-05-06 13:45:17 +0800

    formatted according to vscode style

--- src/skulls.cpp
+++ src/skulls.cpp
@@ -12,9 +12,9 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-const char* instructions = "Virtual Reality Adventure Books are solo adventures with a big difference. They're not random. Whether you live or die doesn't depend on a dice roll -- it's up to you.\n\nTo start your adventure simply choose your character. Each character has a unique selection of four skills: these skills will decide which options are available to you.\n\nAlso note the Life Points and possessions of the character. Life Points are lost each time you are wounded. If you are ever reduced to zero Life Points, you have been killed and the adventure ends. Sometimes you can recover Life Points during the adventure, but you can never have more Life Points that you started with. You can carry up to eight possessions at a time. If you are at this limit and find something else you want, drop one of your other possessions to make room for the new item.\n\nConsider your selection of skills. They establish your special strengths, and will help you to role-play your choices during the adventure. If you arrive at an entry which lists options for more than one of your skills, you can choose which skill to use in that situation.";
+const char *instructions = "Virtual Reality Adventure Books are solo adventures with a big difference. They're not random. Whether you live or die doesn't depend on a dice roll -- it's up to you.\n\nTo start your adventure simply choose your character. Each character has a unique selection of four skills: these skills will decide which options are available to you.\n\nAlso note the Life Points and possessions of the character. Life Points are lost each time you are wounded. If you are ever reduced to zero Life Points, you have been killed and the adventure ends. Sometimes you can recover Life Points during the adventure, but you can never have more Life Points that you started with. You can carry up to eight possessions at a time. If you are at this limit and find something else you want, drop one of your other possessions to make room for the new item.\n\nConsider your selection of skills. They establish your special strengths, and will help you to role-play your choices during the adventure. If you arrive at an entry which lists options for more than one of your skills, you can choose which skill to use in that situation.";
 
-void createWindow(Uint32 flags, SDL_Window** window, SDL_Renderer** renderer, const char* title, int width, int height)
+void createWindow(Uint32 flags, SDL_Window **window, SDL_Renderer **renderer, const char *title, int width, int height)
 {
     // The window we'll be rendering to
     *window = NULL;
@@ -23,16 +23,16 @@ void createWindow(Uint32 flags, SDL_Window** window, SDL_Renderer** renderer, co
     // Initialize SDL
     if (SDL_Init(flags) < 0)
     {
-        std::cerr <<  "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
     }
     else
     {
         // Create window
         SDL_CreateWindowAndRenderer(width, height, SDL_WINDOW_SHOWN, window, renderer);
-        
+
         if (window == NULL || renderer == NULL)
         {
-            std::cerr <<  "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+            std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
         }
         else if (window)
         {
@@ -41,7 +41,7 @@ void createWindow(Uint32 flags, SDL_Window** window, SDL_Renderer** renderer, co
     }
 }
 
-void fillWindow(SDL_Window* window, SDL_Rect* rect, Uint32 color)
+void fillWindow(SDL_Window *window, SDL_Rect *rect, Uint32 color)
 {
     if (window)
     {
@@ -60,7 +60,7 @@ void fillWindow(SDL_Window* window, SDL_Rect* rect, Uint32 color)
     }
 }
 
-void waitForEvent(SDL_Window* window, Uint32 event)
+void waitForEvent(SDL_Window *window, Uint32 event)
 {
     SDL_Event result;
 
@@ -70,7 +70,7 @@ void waitForEvent(SDL_Window* window, Uint32 event)
 
         if (result.type == event)
         {
-           break; 
+            break;
         }
 
         // Update the surface
@@ -78,23 +78,23 @@ void waitForEvent(SDL_Window* window, Uint32 event)
     }
 }
 
-SDL_Surface* loadImage(SDL_Window* window, const char* image)
+SDL_Surface *loadImage(SDL_Window *window, const char *image)
 {
     //Loading success flag
     bool success = true;
 
     //Load splash image
     auto surface = IMG_Load(image);
-    
+
     if (surface == NULL)
     {
-        std::cerr <<  "Unable to load image" << image << "! SDL Error: " << SDL_GetError() << std::endl;
+        std::cerr << "Unable to load image" << image << "! SDL Error: " << SDL_GetError() << std::endl;
     }
 
     return surface;
 }
 
-void renderImage(SDL_Window* window, SDL_Surface* image, int x, int y)
+void renderImage(SDL_Window *window, SDL_Surface *image, int x, int y)
 {
     if (window)
     {
@@ -111,7 +111,7 @@ void renderImage(SDL_Window* window, SDL_Surface* image, int x, int y)
             position.y = y;
 
             SDL_BlitSurface(image, NULL, screen, &position);
-            
+
             SDL_FreeSurface(screen);
 
             screen = NULL;
@@ -119,7 +119,7 @@ void renderImage(SDL_Window* window, SDL_Surface* image, int x, int y)
     }
 }
 
-void renderText(SDL_Window* window, SDL_Surface* text, int x, int y, int bounds, int offset)
+void renderText(SDL_Window *window, SDL_Surface *text, int x, int y, int bounds, int offset)
 {
     if (window)
     {
@@ -142,7 +142,7 @@ void renderText(SDL_Window* window, SDL_Surface* text, int x, int y, int bounds,
             position.y = y;
 
             SDL_BlitSurface(text, &src, screen, &position);
-            
+
             SDL_FreeSurface(screen);
 
             screen = NULL;
@@ -150,7 +150,7 @@ void renderText(SDL_Window* window, SDL_Surface* text, int x, int y, int bounds,
     }
 }
 
-SDL_Surface* createText(const char* text, int font_size, SDL_Color textColor, int wrap)
+SDL_Surface *createText(const char *text, int font_size, SDL_Color textColor, int wrap)
 {
     TTF_Init();
 
@@ -159,32 +159,32 @@ SDL_Surface* createText(const char* text, int font_size, SDL_Color textColor, in
 
     TTF_CloseFont(font);
 
-    font  = NULL;
+    font = NULL;
 
     TTF_Quit();
 
     return surface;
 }
 
-SDL_Surface* createSurface(int width, int height)
+SDL_Surface *createSurface(int width, int height)
 {
     // Create a 32-bit surface with the bytes of each pixel in R,G,B,A order,
     // as expected by OpenGL for textures
     Uint32 rmask, gmask, bmask, amask;
 
-    // SDL interprets each pixel as a 32-bit number, so our masks must depend
-    // on the endianness (byte order) of the machine
-    #if SDL_BYTEORDER == SDL_BIG_ENDIAN
-        rmask = 0xff000000;
-        gmask = 0x00ff0000;
-        bmask = 0x0000ff00;
-        amask = 0x000000ff;
-    #else
-        rmask = 0x000000ff;
-        gmask = 0x0000ff00;
-        bmask = 0x00ff0000;
-        amask = 0xff000000;
-    #endif
+// SDL interprets each pixel as a 32-bit number, so our masks must depend
+// on the endianness (byte order) of the machine
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    rmask = 0xff000000;
+    gmask = 0x00ff0000;
+    bmask = 0x0000ff00;
+    amask = 0x000000ff;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = 0xff000000;
+#endif
 
     return SDL_CreateRGBSurface(0, width, height, 32, rmask, gmask, bmask, amask);
 }
@@ -218,12 +218,12 @@ void displaySplashScreen(SDL_Window *window)
     }
 }
 
-int main(int argc, char** argsv)
+int main(int argc, char **argsv)
 {
     // The window we'll be rendering to
-    SDL_Window* window = NULL;
-    SDL_Renderer* renderer = NULL;
-    
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
     createWindow(SDL_INIT_VIDEO, &window, &renderer, "Necklace of Skulls", SCREEN_WIDTH, SCREEN_HEIGHT);
 
     if (window)
