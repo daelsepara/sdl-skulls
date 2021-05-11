49c598534a41be99820b64afa520ee3214276d6b
Dael Separa <dael.separa@gmail.com>
2021-05-11 17:01:11 +0800

    add chracter checks

--- src/character.hpp
+++ src/character.hpp
@@ -2,6 +2,7 @@
 #define __CHARACTER__HPP__
 #include <vector>
 
+#include "codewords.hpp"
 #include "items.hpp"
 #include "skills.hpp"
 
@@ -18,6 +19,11 @@ namespace Character
 
         std::vector<Skill::Abstract> Skills = std::vector<Skill::Abstract>();
         std::vector<Item::Type> Items = std::vector<Item::Type>();
+        std::vector<Codeword::Type> Codewords = std::vector<Codeword::Type>();
+
+        Abstract()
+        {
+        }
 
         Abstract(const char *name, const char *description, std::vector<Skill::Abstract> skills, std::vector<Item::Type> items, int money)
         {
@@ -37,6 +43,163 @@ namespace Character
     auto ACOLYTE = Abstract("The Acolyte", "You are master of many skills, but you know it is the god who shape man's destiny.", {Skill::ETIQUETTE, Skill::FOLKLORE, Skill::SPELLS, Skill::SWORDPLAY}, {Item::Type::MAGIC_WAND, Item::Type::SWORD}, 10);
     auto SORCERER = Abstract("The Sorcerer", "Born into a high clan, you were schooled in sorcery by priests and wise men. Now you can twist reality itself to suit your wishes.", {Skill::CHARMS, Skill::ETIQUETTE, Skill::ROGUERY, Skill::SPELLS}, {Item::Type::MAGIC_AMULET, Item::Type::MAGIC_WAND}, 10);
 
+    std::vector<Character::Abstract> Classes = {WARRIOR, HUNTER, MYSTIC, WAYFARER, MERCHANT, ACOLYTE, SORCERER};
+
+    bool VERIFY_ITEM(Character::Abstract character, Item::Type item)
+    {
+        auto found = false;
+
+        if (character.Items.size() > 0)
+        {
+            for (auto i = 0; i < character.Items.size(); i++)
+            {
+                if (character.Items[i] == item)
+                {
+                    found = true;
+
+                    break;
+                }
+            }
+        }
+
+        return found;
+    }
+
+    bool VERIFY_SKILL(Character::Abstract character, Skill::Type skill)
+    {
+        auto found = false;
+
+        if (character.Skills.size() > 0 && skill != Skill::Type::NONE)
+        {
+            for (auto i = 0; i < character.Skills.size(); i++)
+            {
+                if (character.Skills[i].Type == skill)
+                {
+                    if (character.Skills[i].Requirement != Item::Type::NONE)
+                    {
+                        found = VERIFY_ITEM(character, character.Skills[i].Requirement);
+                    }
+                    else
+                    {
+                        found = true;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return found;
+    }
+
+    // verify that player has the skill and ANY of the items
+    bool VERIFY_SKILL_ANY(Character::Abstract character, Skill::Type skill, std::vector<Item::Type> items)
+    {
+        auto found = false;
+
+        if (character.Skills.size() > 0 && skill != Skill::Type::NONE)
+        {
+            for (auto i = 0; i < character.Skills.size(); i++)
+            {
+                if (character.Skills[i].Type == skill)
+                {
+                    for (auto j = 0; j < items.size(); j++)
+                    {
+                        found = VERIFY_ITEM(character, items[j]);
+
+                        if (found)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                if (found)
+                {
+                    break;
+                }
+            }
+        }
+
+        return found;
+    }
+
+    // verify that player has the skill and ALL of the items
+    bool VERIFY_SKILL_ALL(Character::Abstract character, Skill::Type skill, std::vector<Item::Type> items)
+    {
+        auto found = 0;
+
+        if (character.Skills.size() > 0 && skill != Skill::Type::NONE)
+        {
+            for (auto i = 0; i < character.Skills.size(); i++)
+            {
+                if (character.Skills[i].Type == skill)
+                {
+                    auto result = std::find(character.Items.begin(), character.Items.end(), items[i]);
+
+                    if (result != character.Items.end())
+                    {
+                        found++;
+                    }
+                }
+            }
+        }
+
+        return found == items.size();
+    }
+
+    bool VERIFY_SKILL_ITEM(Character::Abstract character, Skill::Type skill, Item::Type item)
+    {
+        return VERIFY_SKILL_ALL(character, skill, {item});
+    }
+
+    bool VERIFY_CODEWORDS_ANY(Character::Abstract character, std::vector<Codeword::Type> codewords)
+    {
+        auto found = 0;
+
+        if (character.Codewords.size() > 0 && codewords.size() > 0)
+        {
+            for (auto i = 0; i < codewords.size(); i++)
+            {
+                auto result = std::find(character.Codewords.begin(), character.Codewords.end(), codewords[i]);
+
+                if (result != character.Codewords.end())
+                {
+                    found++;
+                }
+            }
+        }
+
+        return found > 0;
+    }
+
+    bool VERIFY_CODEWORDS_ALL(Character::Abstract character, std::vector<Codeword::Type> codewords)
+    {
+        auto found = 0;
+
+        if (character.Codewords.size() > 0 && codewords.size() > 0)
+        {
+            for (auto i = 0; i < codewords.size(); i++)
+            {
+                auto result = std::find(character.Codewords.begin(), character.Codewords.end(), codewords[i]);
+
+                if (result != character.Codewords.end())
+                {
+                    found++;
+                }
+            }
+        }
+
+        return found == codewords.size();
+    }
+
+    bool VERIFY_CODEWORD(Character::Abstract character, Codeword::Type codeword)
+    {
+        return VERIFY_CODEWORDS_ALL(character, {codeword});
+    }
+
 } // namespace Character
+
+Character::Abstract Player;
 
 #endif
---
+++ src/codewords.hpp
@@ -0,0 +1,27 @@
+#ifndef __CODEWORDS__HPP__
+#define __CODEWORDS__HPP__
+
+namespace Codeword
+{
+    enum class Type
+    {
+        ANGEL,
+        BLESSING,
+        CALABASH,
+        CENOTE,
+        EB,
+        IGNIS,
+        OLMEK,
+        PAKAL,
+        POKTAPOK,
+        PSYCHODUCT,
+        SAKBE,
+        SALVATION,
+        SHADE,
+        VENUS,
+        ZAZ,
+        ZOTZ
+    };
+
+} // namespace Codeword
+#endif
